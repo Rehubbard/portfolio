2aff877a3daed8f3effdbd2d4d428be8db036e81
Eric Hubbard <rehubbard2@gmail.com>
2019-06-24 09:36:11 -0500

    Favicon and google analytics

--- src/pages/index.js
+++ src/pages/index.js
@@ -100,9 +100,9 @@ class HomeIndex extends React.Component {
                   <div className="project-description">
                     <h4>Prometheus web & mobile app</h4>
                     <p>
-                      Built with React Native, the app gives you control over
-                      your trail camera(s). View pictures/videos captured,
-                      manage camera settings & more.
+                      Built with React Native & React, these apps give you
+                      control over your trail camera(s). View pictures/videos
+                      captured, manage camera settings & more.
                     </p>
                   </div>
                 </div>
@@ -149,73 +149,24 @@ class HomeIndex extends React.Component {
           <section id="three">
             <h2>Get In Touch</h2>
             <p>
-              Accumsan pellentesque commodo blandit enim arcu non at amet id
-              arcu magna. Accumsan orci faucibus id eu lorem semper nunc nisi
-              lorem vulputate lorem neque lorem ipsum dolor.
+              Reach out anytime for questions or anything else. I'm always down
+              to get coffee & chat about code, technology, ideas, sports,
+              whatever.
             </p>
-            <div className="row">
-              <div className="8u 12u$(small)">
-                <form method="post" action="#">
-                  <div className="row uniform 50%">
-                    <div className="6u 12u$(xsmall)">
-                      <input
-                        type="text"
-                        name="name"
-                        id="name"
-                        placeholder="Name"
-                      />
-                    </div>
-                    <div className="6u 12u$(xsmall)">
-                      <input
-                        type="email"
-                        name="email"
-                        id="email"
-                        placeholder="Email"
-                      />
-                    </div>
-                    <div className="12u">
-                      <textarea
-                        name="message"
-                        id="message"
-                        placeholder="Message"
-                        rows="4"
-                      ></textarea>
-                    </div>
-                  </div>
-                </form>
-                <ul className="actions">
-                  <li>
-                    <input type="submit" value="Send Message" />
-                  </li>
-                </ul>
-              </div>
-              <div className="4u 12u$(small)">
-                <ul className="labeled-icons">
-                  <li>
-                    <h3 className="icon fa-home">
-                      <span className="label">Address</span>
-                    </h3>
-                    1234 Somewhere Rd.
-                    <br />
-                    Nashville, TN 00000
-                    <br />
-                    United States
-                  </li>
-                  <li>
-                    <h3 className="icon fa-mobile">
-                      <span className="label">Phone</span>
-                    </h3>
-                    000-000-0000
-                  </li>
-                  <li>
-                    <h3 className="icon fa-envelope-o">
-                      <span className="label">Email</span>
-                    </h3>
-                    <a href="#">hello@untitled.tld</a>
-                  </li>
-                </ul>
-              </div>
-            </div>
+            <ul className="labeled-icons">
+              <li>
+                <h3 className="icon fa-envelope-o">
+                  <span className="label">Email</span>
+                </h3>
+                <a href="mailto:rehubbard2@gmail.com">rehubbard2@gmail.com</a>
+              </li>
+              <li>
+                <h3 className="icon fa-mobile">
+                  <span className="label">Phone</span>
+                </h3>
+                205-515-2777
+              </li>
+            </ul>
           </section>
         </div>
       </MainLayout>
